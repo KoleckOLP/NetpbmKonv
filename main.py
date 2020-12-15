e92983541b4c28ba52bf3c4d63b3daa5b050d075
KoleckOLP-work <o.kurz@atlas.cz>
2020-12-15 15:17:21 +0100

    unfinished

--- main.py
+++ main.py
@@ -1,8 +1,94 @@
-import os
+import os, sys
 
-print("write path to a pbm, pgm, ppm")
-picture = input("#")
+try:
+    Finput = sys.argv[1]
+    FinputExt = os.path.splitext(Finput)[1]
+except IndexError:
+    Finput = ""
+    FinputExt = ""
 
+try:
+    Foutput = sys.argv[1]
+    FoutputExt = os.path.splitext(Foutput)[1]
+except IndexError:
+    Foutput = ""
+    FoutputExt = ""
+
+def Ahelp():
+    print("NetpbmKonv v0.9 by KoleckOLP, MobilePony Inc.\n\n" +
+          "main.py \"input_file\" \"output_file\"\n\n" +
+          "supported: .pbm, .pgm, .ppm (all in ASCII only)")
+
+def Fconvert(i="", o=""): #i says the input type, o output type 
+    PicData = []
+    EditedData = []
+
+    f = open(Finput, "r")
+    lines = f.read().splitlines()
+
+    f2 = open(Foutput, "w")
+    f2.write("P2\n# Jsem mega funny KoleckOLP 2020 (C) Make it stop.\n")
+    if "#" in lines[1]: #mrd tam má koment
+        f2.write(lines[2]+" 255"+"\n")
+        for i, line in enumerate(lines):
+            if i > 2:
+                PicData = PicData + [line]
+    else: #nemá takm koment :3
+        f2.write(lines[1]+"\n")
+        for i, line in enumerate(lines):
+            if i > 1:
+                PicData = PicData + [line]
+    for i, data in enumerate(PicData):
+        for c in PicData[i]:
+            if c == "1":
+                output = "0\n"
+            else:
+                output = "255\n"
+            EditedData = EditedData+[output]
+    for data in EditedData:
+        f2.write(data)
+    f2.close()
+
+if Finput != "" and Foutput !="":
+    if "pbm" in FinputExt.lower():
+        if "pgm" in FoutputExt.lower():
+            Fconvert(FinputExt, FoutputExt)
+        elif "ppm" in Foutput.lower():
+            Fconvert(FinputExt, FoutputExt)
+        else:
+            print("wrong wrong output")
+            Ahelp()
+    elif "pgm" in FinputExt.lower():
+        if "pbm" in FoutputExt.lower():
+            Fconvert(FinputExt, FoutputExt)
+        elif "ppm" in Foutput.lower():
+            Fconvert(FinputExt, FoutputExt)
+        else:
+            print("wrong wrong output")
+            Ahelp()
+    elif "ppm" in FinputExt.lower():
+        if "pbm" in FoutputExt.lower():
+            Fconvert(FinputExt, FoutputExt)
+        elif "pgm" in Foutput.lower():
+            Fconvert(FinputExt, FoutputExt)
+        else:
+            print("wrong wrong output")
+            Ahelp()
+    else:
+        print("wrong input")
+        Ahelp()
+else:
+    Ahelp()
+
+
+
+
+
+
+#print("write path to a pbm, pgm, ppm")
+#picture = input("#")
+
+'''
 f = open(picture, "r")
 
 lines = f.read().splitlines()
@@ -205,3 +291,4 @@ if "P3" in lines[0]:
         for data in EditedData:
             f2.write(data)
         f2.close()
+'''
