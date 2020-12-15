fb930b47d1fa5d0ff7e710b02b865d2e93f0cb76
KoleckOLP-work <o.kurz@atlas.cz>
2020-12-15 07:01:01 +0100

    pgm to ppm

--- main.py
+++ main.py
@@ -96,13 +96,34 @@ if "P2" in lines[0]:
                 output = "1"
             elif int(data) > 128 and int(data) < 256:
                 output = "0"
+            else:
+                output = ""
             j=j+1
             pbmline = pbmline + output
         for data in EditedData:
             f2.write(data)
         f2.close()
     elif cmd == "2": #PPM
-        pass
+        name = os.path.splitext(picture)[0]
+        name = name+"_pgm.ppm"
+        f2 = open(name, "w")
+        f2.write("P3\n# Jsem mega funny KoleckOLP 2020 (C) Make it stop.\n")
+        if "#" in lines[1]: #mrd tam má koment
+            f2.write(lines[2]+" 255"+"\n")
+            for i, line in enumerate(lines):
+                if i > 3:
+                    PicData = PicData + [line]
+        else: #nemá takm koment :3
+            f2.write(lines[1]+"\n")
+            for i, line in enumerate(lines):
+                if i > 2:
+                    PicData = PicData + [line]
+        for i, data in enumerate(PicData):
+            output = f"{data}\n{data}\n{data}\n"
+            EditedData = EditedData+[output]
+        for data in EditedData:
+            f2.write(data)
+        f2.close()
 
 if "P3" in lines[0]:
     print("1. to pbm 2. to pgm")
